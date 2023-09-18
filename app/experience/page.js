c85925c50df958d35a154bbf2fbaeeebc9b71da0
Allen Nguyen <anhn321@gmail.com>
2023-09-18 01:14:41 -0700

    Add description to experiences

--- app/experience/page.js
+++ app/experience/page.js
@@ -5,7 +5,7 @@ import { FaChalkboardTeacher } from 'react-icons/fa';
 
 export default function Experience() {
   return (
-    <main className='flex flex-col flex-auto items-center m-8 gap-8'>
+    <main className='flex flex-col flex-auto items-center m-8 mb-2 gap-12'>
       <h1>experience 💻</h1>
 
       <ol className='relative border-l border-gray-200 dark:border-gray-700'>
@@ -13,27 +13,32 @@ export default function Experience() {
           position='Software Engineer'
           company='Voronoi Health Analytics • Co-op'
           time='Sept 2023 - Present'
+          description='🩻 medical imaging processing and AI software'
         />
         <TimelineItem
           position='Software Developer'
           company='UBC Launchpad • Seasonal'
           time='Sept 2023 - Present'
+          description='👨🏻‍⚕️ seizure tracking mobile application'
         />
         <TimelineItem
           position='Software Engineer'
           company='BCI • Co-op'
           time='Sept 2022 - Dec 2022'
+          description='📈 internal data collection full stack web application'
         />
         <TimelineItem
           position='Engineering Projects Instructor'
           company='Zen Maker Lab • Co-op'
           time='Jan 2022 - Aug 2022'
+          description='🧑🏻‍🏫 STEM education in coding, science, and engineering'
           icon={<FaChalkboardTeacher />}
         />
         <TimelineItem
           position='Renewable Energy Team Lead'
           company='UBC WasteNauts • Seasonal'
           time='May 2021 - April 2022'
+          description='🔋 sustainable renewable energy harvesting system'
           icon={<PiPottedPlantBold />}
         />
       </ol>
@@ -49,7 +54,7 @@ function TimelineItem({
   icon = <BsPersonWorkspace />,
 }) {
   return (
-    <li className='mb-10 ml-6'>
+    <li className='mb-8 ml-6'>
       <span className='absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900'>
         {icon}
       </span>
