b0bd4c13534538a20170a10c20d216fd6b5dc730
Allen Nguyen <anhn321@gmail.com>
2024-02-09 14:18:22 -0800

    Update resume Feb 2024

--- app/layout.js
+++ app/layout.js
@@ -8,36 +8,54 @@ import miiHeadshot from './icon.png';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Allen Nguyen',
-  description: "Allen Nguyen's portfolio site",
+	title: 'Allen Nguyen',
+	description: "Allen Nguyen's portfolio site",
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang='en'>
-      <body className={`${inter.className} flex flex-col min-h-screen`}>
-        <nav className='flex justify-between items-center gap-4 my-4 mx-4 lg:mx-56 text-lg font-medium'>
-          <Link href='/' className='flex items-center gap-2'>
-            <button className='btn btn-circle btn-neutral w-16 h-16'>
-              <Image
-                src={miiHeadshot}
-                alt='Allen as a Mii'
-                className='w-14 rounded-full'
-              />
-            </button>
-          </Link>
-          <section className='flex gap-4'>
-            <Link href='/projects'>projects</Link>
-            <Link href='/experience'>experience</Link>
-          </section>
-        </nav>
-        {children}
+	return (
+		<html lang='en'>
+			<body className={`${inter.className} flex flex-col min-h-screen`}>
+				<nav className='flex justify-between items-center gap-4 my-4 mx-4 lg:mx-56 text-lg font-medium'>
+					<Link href='/' className='flex items-center gap-2'>
+						<button className='btn btn-circle btn-neutral w-16 h-16'>
+							<Image
+								src={miiHeadshot}
+								alt='Allen as a Mii'
+								className='w-14 rounded-full'
+							/>
+						</button>
+					</Link>
+					<section className='flex gap-2 justify-center items-center'>
+						<Link
+							href='/projects'
+							className='hover:bg-black px-4 py-2 rounded-lg'
+						>
+							projects
+						</Link>
+						<Link
+							href='/experience'
+							className='hover:bg-black px-4 py-2 rounded-lg'
+						>
+							experience
+						</Link>
+						<Link
+							href='./Allen Nguyen Resume.pdf'
+							className='bg-accent px-4 py-2 rounded-lg text-black hover:bg-accent-focus'
+							target='_blank'
+							rel='noopener noreferrer'
+						>
+							resume
+						</Link>
+					</section>
+				</nav>
+				{children}
 
-        <footer className='flex flex-col text-sm m-4 text-center gap-4'>
-          <hr />
-          <p>Ⓒ 2023 Allen Nguyen, Vancouver Canada</p>
-        </footer>
-      </body>
-    </html>
-  );
+				<footer className='flex flex-col text-sm m-4 text-center gap-4'>
+					<hr />
+					<p>Ⓒ 2023 Allen Nguyen, Vancouver Canada</p>
+				</footer>
+			</body>
+		</html>
+	);
 }
--- components/ContactIcons.js
+++ components/ContactIcons.js
@@ -2,54 +2,54 @@
 
 import { motion } from 'framer-motion';
 import {
-  RiFilePaper2Fill,
-  RiGithubFill,
-  RiLinkedinFill,
-  RiMailFill,
+	RiFilePaper2Fill,
+	RiGithubFill,
+	RiLinkedinFill,
+	RiMailFill,
 } from 'react-icons/ri';
 
 export default function ContactIcons() {
-  const iconSize = 50;
+	const iconSize = 50;
 
-  const contacts = [
-    {
-      name: 'linkedIn',
-      icon: <RiLinkedinFill className='w-12 h-12 lg:w-16 lg:h-16' />,
-      link: 'https://www.linkedin.com/in/allendnguyen/',
-    },
-    {
-      name: 'github',
-      icon: <RiGithubFill className='w-12 h-12 lg:w-16 lg:h-16' />,
-      link: 'https://github.com/allennguyen01/',
-    },
-    {
-      name: 'resume',
-      icon: <RiFilePaper2Fill className='w-12 h-12 lg:w-16 lg:h-16' />,
-      link: './resume.pdf',
-    },
-    {
-      name: 'email',
-      icon: <RiMailFill className='w-12 h-12 lg:w-16 lg:h-16' />,
-      link: 'mailto:allennguyen415@gmail.com',
-    },
-  ];
+	const contacts = [
+		{
+			name: 'linkedIn',
+			icon: <RiLinkedinFill className='w-12 h-12 lg:w-16 lg:h-16' />,
+			link: 'https://www.linkedin.com/in/allendnguyen/',
+		},
+		{
+			name: 'github',
+			icon: <RiGithubFill className='w-12 h-12 lg:w-16 lg:h-16' />,
+			link: 'https://github.com/allennguyen01/',
+		},
+		{
+			name: 'resume',
+			icon: <RiFilePaper2Fill className='w-12 h-12 lg:w-16 lg:h-16' />,
+			link: './Allen Nguyen Resume.pdf',
+		},
+		{
+			name: 'email',
+			icon: <RiMailFill className='w-12 h-12 lg:w-16 lg:h-16' />,
+			link: 'mailto:allennguyen415@gmail.com',
+		},
+	];
 
-  return (
-    <div className='flex gap-4'>
-      {contacts.map((contact) => (
-        <div key={contact.name} className='flex flex-col items-center'>
-          <motion.a
-            href={contact.link}
-            whileHover={{ rotate: 180 }}
-            whileTap={{ scale: 1.5 }}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            {contact.icon}
-          </motion.a>
-          <p className='text-sm'>{contact.name}</p>
-        </div>
-      ))}
-    </div>
-  );
+	return (
+		<div className='flex gap-4'>
+			{contacts.map((contact) => (
+				<div key={contact.name} className='flex flex-col items-center'>
+					<motion.a
+						href={contact.link}
+						whileHover={{ rotate: 180 }}
+						whileTap={{ scale: 1.5 }}
+						target='_blank'
+						rel='noopener noreferrer'
+					>
+						{contact.icon}
+					</motion.a>
+					<p className='text-sm'>{contact.name}</p>
+				</div>
+			))}
+		</div>
+	);
 }
