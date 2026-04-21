e41225de2a258ea13b2955f52bcb1a74f91273d3
Allen Nguyen <anhn321@gmail.com>
2026-04-21 00:02:06 -0700

    feat: update full-time

--- pages/experience.js
+++ pages/experience.js
@@ -13,6 +13,15 @@ import UCalgaryCSSLogo from '@/public/images/experience/ucalgarycss-logo.png';
 import VSALogo from '@/public/images/experience/vsa-logo.png';
 import UBCWasteNautsLogo from '@/public/images/experience/ubcwastenauts-logo.png';
 
+const currentWork = {
+  position: 'Junior Software Engineer',
+  company: 'Ontraccr Technologies',
+  time: 'Dec. 2025 - Present',
+  description: '🏗️ full-stack construction project management app dev',
+  logo: OntraccrLogo,
+  link: 'https://www.ontraccr.com/',
+};
+
 const workExperience = [
   {
     position: 'Frontend Developer',
@@ -112,6 +121,11 @@ export default function Experience() {
   return (
     <main className='mx-8 mt-24 mb-2 flex flex-auto flex-col items-center gap-12 lg:mt-32'>
       <h1>experience 💻</h1>
+
+      <div>
+        <h2 className='relative left-24 mb-4'>current work</h2>
+        <TimelineItem {...currentWork} />
+      </div>
 
       <div className='flex flex-col gap-8 lg:flex-row lg:gap-16'>
         <div className='flex flex-col gap-4'>
